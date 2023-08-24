927dc83a3644042a023a776d7fc637c38924a770
matheusjbk <matheusjbk@gmail.com>
2023-08-24 02:01:56 -0300

    classe ContaTerminal desenvolvida e add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio DIO - Conta Bancária
+
+Desafio para desenvolver uma Classe em Java que simule uma conta bancária pedindo os dados para o usuário e mostrando-os no terminal.
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+/**
+ * Classe que simula uma conta bancária para o desafio da DIO.
+ * Você pode inserir seus dados que irá ser lançada uma mensagem no Terminal com todos eles.
+ * 
+ * @author Matheus Keher
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome completo:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o número da conta bancária:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o número da agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o saldo da sua conta:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque.");
     }
 }
